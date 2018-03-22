ee3fcee9d9341d3147b95e10f8490b71d2a37c72
Patrick Sadil <psadil@gmail.com>
2018-03-21 20:19:00 -0400

    tools

--- R/drake_utils.R
+++ R/drake_utils.R
@@ -1,37 +1,4 @@
 
-#' @export
-run_stan <- function(stan_data){
-  pars <- c("theta_log", "theta_raw", "lps", "zeta_raw", "theta_raw","condition_mu_raw", "zeta",
-            "Mu_unordered", "Mu_ordered",
-            "subject_mu_raw", "item_mu_raw", "subject_mu",
-            "subject_L", "item_L",
-            "condition_omega")
-
-  model <- stan_model(file.path(devtools::package_file(), "stan", paste0(params$model, ".stan")))
-
-  post <- sampling(model,
-                   data = stan_data,
-                   iter = params$warmup + params$iter,
-                   warmup = params$warmup,
-                   chains = params$chains
-                   , pars = pars
-                   , include = FALSE
-                   , init_r = 0.25
-                   , control = list(adapt_delta = .99)
-  )
-
-  loaded_dlls = getLoadedDLLs()
-  loaded_dlls = loaded_dlls[str_detect(names(loaded_dlls), '^file')]
-  if (length(loaded_dlls) > 10) {
-    for (dll in head(loaded_dlls, -10)) {
-      message("Unloading DLL ", dll[['name']], ": ", dll[['path']])
-      dyn.unload(dll[['path']])
-    }
-  }
-
-
-  return(post)
-}
 
 #' @export
 apply_type <- function(d, model_type){
