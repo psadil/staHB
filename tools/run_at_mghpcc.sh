00945f692e40300f82194b99428173e9731dc06a
psadil <psadil@gmail.com>
2018-10-31 16:25:42 -0400

    unsure

--- tools/run_at_mghpcc.sh
+++ tools/run_at_mghpcc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-module load singularity/singularity-2.4.5
+module load singularity/singularity-current
 
-#BSUB -n 36
+#BSUB -n 48
 #BSUB -R rusage[mem=1024] # ask for 1GB per job slot, or 36GB total
-#BSUB -W 2:00
+#BSUB -W 48:00
 #BSUB -q long # which queue we want to run in
 #BSUB -R "span[hosts=1]" # All job slots on the same node (needed for threaded applications)
 #BSUB -J staHB
 
-singularity exec psadil-staHB-master-latest.simg Rscript -e "library(staHB); setup_job(jobs=6, n_chains=6, type_model = c('nmon','mon','full'), n_radius=3, n_radian=3)"
+singularity exec psadil-staHB-master-latest.simg Rscript -e "library(staHB); cache1 <- drake::new_cache('.cmr'); setup_cart(jobs=6, n_chains=8, type_model =  c('nmon','mon','full'), n_condition_rho = 1, n_item = 20, n_subject = c(20,50), cache_dir = cache1)"
